8fdb6298cabea2c46b1b3d2648d11e0f22f07518
Artem <Skidross@yandex.ru>
2019-06-19 22:51:13 +0300

    Beta 0.2 Profile Edit

--- component/profile/profile.js
+++ component/profile/profile.js
@@ -11,69 +11,65 @@ const Profile = ()=>{
     <ImageBackground source={require('../../img/bgLogin.png')} style={styles.backGround}>
 
 
-
-        <View>
-                <View style={styles.profileHeader}>
-                    <TouchableOpacity onPress={()=>alert('Menu')}>
-                        <Icon name={'ios-menu'}  size={40} color={'rgba(255,255,255,0.7)'}/>
-                    </TouchableOpacity>
-                        <Text style={styles.profileText}>Profile</Text>
-                    <TouchableOpacity onPress={()=>alert('Settings')}>
-                        <Icon name={'ios-settings'}  size={40} color={'rgba(255,255,255,0.7)'}/>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={styles.avatarContainer}>
-                <TouchableOpacity onPress={()=>alert('Insta')}>
-                    <Icon name={'logo-instagram'}  size={40} color={'#ec4a66'}/>
-                </TouchableOpacity>
-                
-                <Image source={require('../../img/Avatar.png')} style={styles.avatar}/>
-                    
-                <TouchableOpacity onPress={()=>alert('Vk')}>
-                    <Icon name={'logo-vk'}  size={40} color={'#4a76a8'}/>
-                </TouchableOpacity>
-                </View>
-
-                <View style={styles.aboutMe}>
-                    <Text>Username</Text>
-                    <View style={styles.inputContainer}>
-                        <Icon name={'ios-person'} size={21} color={'red'}style={styles.inputIcon}/>
-                            <TextInput 
-                            style={styles.input}
-                            placeholder={'Username'}
-                            placeholderTextColo={'rgba(255,255,255,0.4)'}
-                            underlineColorAndroid='transparent'
-                            />
-                    </View>
-            <View style={styles.inputContainer}>
-                <Icon name={'ios-lock'} size={21} color={'red'} style={styles.inputIcon}/>
-                <Text>Password</Text>
-                
-            <TextInput 
-                style={styles.input}
-                placeholder={'Password'}
-                secureTextEntry={false}
-                placeholderTextColo={'rgba(255,255,255,0.4)'}
-                underlineColorAndroid='transparent'/>
-        </View>
-            <View>
-                <Text>
-                    Instagram
-                </Text>
-<Switch onValueChange = {'handlerHere'} value = {'Pre decided value if any'}/>
-            </View>
-
-            <View>
-                <Text>
-                    Instagram
-                </Text>
-<Switch onValueChange = {'handlerHere'} value = {'Pre decided value if any'}/>
-            </View>
-                </View>
+        <View style={styles.header}>
+            <Image source={require('../../img/Avatar.png')} style={styles.headerAvatar}/>
+            <Text style={styles.headerText}>
+                Artem Jhilinskiy
+            </Text>
+            <Text style={styles.location}>
+                <Icon name={'md-pin'} size={24} color={'#fffd'}/>  Russia,Moscow
+            </Text>
         </View>
 
-        
+
+    <View style={styles.headerSocialWeb}>
+
+
+        <View style={styles.socialWeb}>
+            <View style={styles.socialWebSub}>
+
+            <TouchableOpacity>
+            <Icon name={'md-paper'} size={40}  color="#ec4a66" style={styles.block}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+            <Icon name={'ios-mail'} size={40}  color="#ec4a66" style={styles.block}/>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+
+
+        <View style={styles.socialWeb}>
+            <View style={styles.socialWebSub}>
+
+            <TouchableOpacity>
+            <Icon name={'ios-images'} size={40}  color="#008fd5" style={styles.block}/>
+                </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Icon name={'md-people'} size={40}  color="#008fd5" style={styles.block}/>
+            </TouchableOpacity>
+
+            </View>
+        </View>
+
+
+        <View style={styles.socialWeb}>
+            <View style={styles.socialWebSub}>
+
+            <TouchableOpacity>
+                <Icon name={'ios-settings'} size={40}  color="#008fd5" style={styles.block}/>
+            </TouchableOpacity>
+
+                <TouchableOpacity>
+            <Icon name={'ios-headset'} size={40}  color="#ec4a66" style={styles.block}/>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+
+    </View>
         
     </ImageBackground>
     )
@@ -84,51 +80,40 @@ export default Profile
 const styles = StyleSheet.create({
     backGround:{
         width:WIDTH,
-        height:255,
+        height:240
     },
-    profileHeader:{
+    header:{
         display:'flex',
-        flexDirection:'row',
-        justifyContent:'space-between',
         alignItems:'center',
-        paddingTop:20
+        marginTop:35
     },
-    profileText:{
-        fontSize:22,
-        color:'#fff'
-    },
-    avatar:{
+    headerAvatar:{
         width:140,
         height:140,
-        borderRadius:25,
+        borderRadius:100,
     },
-    inputContainer:{
-        marginTop:10,
-        
+    headerText:{
+        color:'#fff',
+        fontSize:17
     },
-    input:{
-        width:WIDTH - 55,
-        height:25,
-        borderRadius:45,
-        fontSize:16,
-        paddingLeft:45,
-        backgroundColor:'rgba(0,0,0,0.35)',
-        color:'rgba(255,255,255,0.7)',
-        marginHorizontal:25
+    location:{
+        color:'#fffa',
+        marginRight:10
     },
-    inputIcon:{
-        position:'absolute',
-        top:2,
-        left:37,
-    },
-    avatarContainer:{
+    socialWebSub:{
         display:'flex',
         flexDirection:'row',
         justifyContent:'space-around',
-        alignItems:'flex-start',
-        paddingTop:125,
+        marginTop:41,
+        alignItems:'center',
     },
-    aboutMe:{
-        
+    socialWeb:{
+        padding:10
+    },
+    headerSocialWeb:{
+        marginTop:16,
+        backgroundColor:'khaki',
+        height:365
     }
+    
 })
