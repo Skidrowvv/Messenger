e5265dd30f58a34e972543c544546b3eb2ccabcc
Artem <Skidross@yandex.ru>
2019-06-19 17:24:12 +0300

    Beta 0.1

---
+++ App.js
@@ -0,0 +1,139 @@
+import React,{Component} from 'react';
+import { createAppContainer, createStackNavigator,initialRouteName, StackActions, NavigationActions,createBottomTabNavigator  } from 'react-navigation';
+import { StyleSheet,TouchableOpacity,Dimensions, Text, View,ImageBackground,Image,TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+const {width:WIDTH} = Dimensions.get('window');
+
+export default class Login extends Component{
+  constructor(){
+    super()
+    this.state ={
+      showPass:true,
+      press:false
+    }
+  }
+
+  showPass = ()=>{
+    if(this.state.press == false){
+      this.setState({showPass:false,press:true})
+    }else{
+      this.setState({showPass:true,press:false})
+    }
+  }
+
+  render(){
+    return (
+      <ImageBackground source={require('./img/Group_3.png')} style={styles.backgroundContainer}>
+          
+          <View style={styles.logoContainer}>
+            <Image style={styles.logo} source={require('./img/logo2.png')} />
+            <Text style={styles.logoText}>Welcome to Messenger</Text>
+          </View>
+  
+          <View style={styles.inputContainer}>
+            <Icon name={'ios-person'} size={28} color={'rgba(255,255,255,0.7)'}
+              style={styles.inputIcon}
+            />
+  
+            <TextInput 
+            style={styles.input}
+              placeholder={'Username'}
+              placeholderTextColo={'rgba(255,255,255,0.7)'}
+              underlineColorAndroid='transparent'
+            />
+          </View>
+  
+          
+          <View style={styles.inputContainer}>
+          <Icon name={'ios-lock'} size={28} color={'rgba(255,255,255,0.7)'}
+            style={styles.inputIcon}
+          />
+  
+            <TextInput 
+            style={styles.input}
+              placeholder={'Password'}
+              secureTextEntry={this.state.showPass}
+              placeholderTextColo={'rgba(255,255,255,0.7)'}
+              underlineColorAndroid='transparent'
+            />
+  
+          <TouchableOpacity style={styles.btnEye}
+            onPress={this.showPass.bind(this)}>
+              <Icon name={this.state.press == false ? 'ios-eye' :'ios-eye-off'}  size={26} color={'rgba(255,255,255,0.7)'}/>
+          </TouchableOpacity>
+          </View>
+  
+          <TouchableOpacity style={styles.btnLogin}>
+            <Text style={styles.text}>Login</Text>
+          </TouchableOpacity>
+      </ImageBackground>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    width:null,
+    height:null,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer:{
+    alignItems:'center',
+    marginBottom:50
+  },
+  logo:{
+    width:120,
+    height:120
+  },
+  logoText:{
+    color:'#fff',
+    fontSize:20,
+    fontWeight:'500',
+    marginTop:10,
+    opacity:0.5
+  },
+  inputContainer:{
+    marginTop:10,
+    
+  },
+  input:{
+    width:WIDTH - 55,
+    height:45,
+    borderRadius:45,
+    fontSize:16,
+    paddingLeft:45,
+    backgroundColor:'rgba(0,0,0,0.35)',
+    color:'rgba(255,255,255,0.7)',
+    marginHorizontal:25
+  },
+  inputIcon:{
+    position:'absolute',
+    top:8,
+    left:37,
+  },
+  btnEye:{
+    position:'absolute',
+    right:37,
+    top:8
+  },
+  btnLogin:{
+    width:WIDTH - 55,
+    height:45,
+    borderRadius:45,
+    backgroundColor:'#ff2d55',
+    justifyContent:'center',
+    marginTop:20,
+  },
+  text:{
+    color:'rgba(255,255,255,0.7 )',
+    fontSize:16,
+    textAlign:'center',
+  }
+});
+
